9aee47fcb7d8dbe9ad644413ef2fded09bc0b1d4
Manuel Rauber <manuel.rauber@thinktecture.com>
2017-09-19 08:46:16 +0200

    live coding index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,1 @@
 import 'reflect-metadata';
-import {ApiServer} from './server/index';
-import {DatabaseProvider} from './database/index';
-
-DatabaseProvider.configure({
-    type: process.env.DATABASE_TYPE as any || 'postgres',
-    database: process.env.DATABASE_NAME || 'ijs2017',
-    username: process.env.DATABASE_USERNAME || 'ijs2017',
-    password: process.env.DATABASE_PASSWORD || 'ijs2017',
-    host: process.env.DATABASE_HOST || 'localhost',
-    port: +process.env.DATABASE_PORT || 5432,
-    ssl: !!process.env.USE_SSL
-});
-
-const server = new ApiServer();
-server.start(+process.env.PORT || 8080);
