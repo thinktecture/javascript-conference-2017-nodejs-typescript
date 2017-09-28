87882b07d88c6338f3fb92f87cfa7fcbb4937208
Manuel Rauber <manuel.rauber@thinktecture.com>
2017-09-28 09:37:14 +0200

    corrects credentials

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import {DatabaseProvider} from './database/index';
 
 DatabaseProvider.configure({
     type: process.env.DATABASE_TYPE as any || 'postgres',
-    database: process.env.DATABASE_NAME || '﻿basta2017',
-    username: process.env.DATABASE_USERNAME || '﻿basta2017',
-    password: process.env.DATABASE_PASSWORD || '﻿basta2017',
+    database: process.env.DATABASE_NAME || 'basta2017',
+    username: process.env.DATABASE_USERNAME || 'basta2017',
+    password: process.env.DATABASE_PASSWORD || 'basta2017',
     host: process.env.DATABASE_HOST || 'localhost',
     port: +process.env.DATABASE_PORT || 5432,
     ssl: !!process.env.USE_SSL
